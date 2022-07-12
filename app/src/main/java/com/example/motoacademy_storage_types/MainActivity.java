e22e93d4f99cc7b0271ffce2e2489e6da20dc475
Denis Rosas <denis.rosas@gmail.com>
2022-07-11 23:39:52 -0300

    Adjustment of some string values to support portuguese and English. Added the SystemProperties class to read the properties. Added some checks of empty values

--- app/src/main/java/com/example/motoacademy_storage_types/MainActivity.java
+++ app/src/main/java/com/example/motoacademy_storage_types/MainActivity.java
@@ -16,12 +16,7 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.Set;
 
-
-
 public class MainActivity extends AppCompatActivity {
-
-    //String propName = "dev.MotoAcademy.property";
-    String propName = "dev.motorola.motoacademy";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,16 +39,16 @@ public class MainActivity extends AppCompatActivity {
         if (!myTextField.getText().toString().equals("")) {
             sharedPreferences.edit().putString("textData", myTextField.getText().toString()).apply();
             myTextField.setText("");
-            Toast.makeText(getApplicationContext(), "Shared Preferences stored", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Shared Preferences stored", Toast.LENGTH_SHORT).show();
         } else {
-            Toast.makeText(getApplicationContext(), "Shared Preferences field is empty", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Shared Preferences field is empty", Toast.LENGTH_SHORT).show();
         }
     }
 
     public void ReadSharedPreferences(View view){
         SharedPreferences sharedPreferences = this.getSharedPreferences("com.example.motoacademy_storage_types0", MODE_PRIVATE);
         String textData = sharedPreferences.getString("textData", "");
-        Toast.makeText(getApplicationContext(), "Value from Shared Preferences is "+textData, Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), "Value from Shared Preferences is "+textData, Toast.LENGTH_SHORT).show();
     }
 
     public void StoreInDatabase(View view){
@@ -61,46 +56,53 @@ public class MainActivity extends AppCompatActivity {
         String name = ((EditText)findViewById(R.id.editTextPersonName)).getText().toString();
         String phoneNumber = ((EditText)findViewById(R.id.editTextPhone)).getText().toString();
 
-        String query = "INSERT INTO users (name, phone) VALUES ('"+
-                name+"', '" +
-                phoneNumber+"')";
+        if (name.equals("") | phoneNumber.equals("")){
+            Toast.makeText(getApplicationContext(), "One or more fields is empty", Toast.LENGTH_SHORT).show();
+        } else{
+            String query = "INSERT INTO users (name, phone) VALUES ('"+
+                    name+"', '" +
+                    phoneNumber+"')";
+
+            try {
+                sqLiteDatabase.execSQL(query);
+                Toast.makeText(getApplicationContext(), "Values stored in database", Toast.LENGTH_SHORT).show();
+            } catch (Exception e){
+                e.printStackTrace();
+                Toast.makeText(getApplicationContext(), "Failed to store values in database", Toast.LENGTH_SHORT).show();
+            }
+
+            Log.i("StoreInDatabase", "Query: "+query);
+        }
+    }
+
+    public void ClearDatabaseTable(View view){
+        SQLiteDatabase sqLiteDatabase = this.openOrCreateDatabase("MotoAcademy", MODE_PRIVATE, null);
+
+        String query = "DELETE FROM users";
 
         try {
             sqLiteDatabase.execSQL(query);
-            Toast.makeText(getApplicationContext(), "Values stored in database", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "All values were deleted from table users", Toast.LENGTH_SHORT).show();
         } catch (Exception e){
             e.printStackTrace();
-            Toast.makeText(getApplicationContext(), "Failed to store values in database", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Failed to store values in database", Toast.LENGTH_SHORT).show();
         }
 
         Log.i("StoreInDatabase", "Query: "+query);
 
     }
 
-    public void WriteProperty(View view){
-        String newPropValue = ((EditText)findViewById(R.id.propNewValue)).getText().toString();
-        if (newPropValue.equals("")){
-            Toast.makeText(getApplicationContext(), "Prop field is empty!!", Toast.LENGTH_LONG).show();
-        } else {
-            try {
-                System.setProperty(propName, newPropValue);
-            } catch (Exception e){
-                e.printStackTrace();
-            }
-            Toast.makeText(getApplicationContext(), "Property "+propName+ " has now new value "+newPropValue+".", Toast.LENGTH_LONG).show();
-        }
-    }
-
     public void ReadProperty(View view){
-        //String propValue = "denis"+System.getProperty(propName);
-        Properties properties = System.getProperties();
+        String propName = "dev.motoacademy.value";
+        SystemProperties properties = new SystemProperties();
+        String propValue = SystemProperties.read(propName);
 
-        Log.i("MotoTest", properties.getProperty(propName));
+        Log.i("MotoAcademy", "Property "+propName+" has value of "+propValue);
 
-        if (properties.getProperty(propName).equals("")){
+        if (propValue.equals("")){
             Toast.makeText(getApplicationContext(), "Prop is empty!!", Toast.LENGTH_LONG).show();
         } else{
-            Toast.makeText(getApplicationContext(), "Property "+propName+ " has value of "+properties.getProperty(propName)+".", Toast.LENGTH_LONG).show();
+            Toast.makeText(getApplicationContext(), "Property "+propName+ " has value of "+propValue+".", Toast.LENGTH_SHORT).show();
         }
 
     }
